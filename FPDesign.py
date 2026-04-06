7140dd564b84f0e1cd5a38ace0fa0b38b744f3b4
melissayuan1 <melissayuan1@college.harvard.edu>
2026-04-06 23:37:01 +0000

    Mon, Apr 6, 2026, 7:37 PM -04:00

--- FPDesign.py
+++ FPDesign.py
@@ -1,7 +1,17 @@
 #The step we are tackling for this assignment is the taking the input of a mode of transportation
 #(car, walking, bking etc.) and then a distance travelled, and finding the kg of CO2 produced
 
-# Take user input from
+# List of valid options
+
+transportation = ['car', 'bike', 'walk', 'train', 'plane']
+
+# Let user input their data
+my_transportation = input('How did you travel today').lower()
+
+if my_transportation in transportation:
+    break
+else:
+    print('Shape must be rock, paper, or scissors. Try again...')
 
 
 
