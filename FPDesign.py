910775acb16817d1fd4d9fc27019c00217f206a9
melissayuan1 <melissayuan1@college.harvard.edu>
2026-04-06 23:34:00 +0000

    Mon, Apr 6, 2026, 7:34 PM -04:00

---
+++ FPDesign.py
@@ -0,0 +1,6 @@
+#The step we are tackling for this assignment is the taking the input of a mode of transportation
+#(car, walking, bking etc.) and then a distance travelled, and finding the kg of CO2 produced
+
+# Take user input
+
+
