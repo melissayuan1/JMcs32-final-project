78619bf3d6a574a0f88eaf9009281282ca772b3e
jmohiuddin05 <jmohiuddin@college.harvard.edu>
2026-04-19 00:25:29 +0000

    Sat, Apr 18, 2026, 8:25 PM -04:00

--- updatedFPstatus.py
+++ updatedFPstatus.py
@@ -1,23 +1,30 @@
 # main.py
-
 from modules.dataload import load_data
 from modules.inputs import get_valid_input, get_float
 from modules.recs import get_recommendations
 from modules.calculate import calculate_transport
 from modules.calculate import calculate_water
 
+#intial print statement
+print("##CARBON FOOTPRINT CALCULATOR BASED ON TRANSPORTATION, ENERGY TYPE, AND WATER USAGE##")
+
 # Load data
 transport_data = load_data('data/transport.csv', 'transportation', 'emissions')
 energy_data = load_data('data/energytype.csv', 'energy_source', 'emissions')
 
+
 # user inputs
-transport = get_valid_input("Transport: ", transport_data.keys())
-energy = get_valid_input("Energy: ", energy_data.keys())
+transport = get_valid_input("Mode of transportation: ", transport_data.keys())
 distance = get_float("Miles per day: ")
+transport_emissions = calculate_transport(transport_data, transport, distance)
+print(f'Transportation emissions: {transport_emissions }')
+
+
+energy = get_valid_input("Energy type used: ", energy_data.keys())
 shower_time = get_float("Minutes per shower: ")
 
 # calc
-transport_emissions = calculate_transport(transport_data, transport, distance)
+
 water_emissions = calculate_water(shower_time, 0.2)
 
 total = transport_emissions + water_emissions
