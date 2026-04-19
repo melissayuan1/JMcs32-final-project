3700b9224bd8b0d46186ae3f57168a6bc0382886
jmohiuddin05 <jmohiuddin@college.harvard.edu>
2026-04-19 01:21:56 +0000

    Sat, Apr 18, 2026, 9:21 PM -04:00

--- modules/recs.py
+++ modules/recs.py
@@ -7,10 +7,10 @@ def get_recommendations(inputs):
     if inputs["meat_emissions"] > 0.67:
         recs.append("Reducing beef consumption can have a huge impact.")
 
-    if inputs["shower_time"] > 10:
+    if inputs["shower_time"] > 7:
         recs.append("Shorten showers to save water and energy.")
 
     if not recs:
-        recs.append("Keep up the great work. Here is a link to more suggestions to reduce your carbon footprint: https://www.un.org/en/actnow/ten-actions ")
+        recs.append("Keep up the great work! Here is a link to more suggestions to reduce your carbon footprint: https://www.un.org/en/actnow/ten-actions ")
 
     return recs
