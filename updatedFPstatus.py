636a218126b70ad675783528fe3ee38b27cae0f2
jmohiuddin05 <jmohiuddin@college.harvard.edu>
2026-04-19 00:37:06 +0000

    Sat, Apr 18, 2026, 8:37 PM -04:00

--- updatedFPstatus.py
+++ updatedFPstatus.py
@@ -13,21 +13,23 @@ transport_data = load_data('data/transport.csv', 'transportation', 'emissions')
 energy_data = load_data('data/energytype.csv', 'energy_source', 'emissions')
 
 
-# user inputs
+# user inputs - trans
 transport = get_valid_input("Mode of transportation: ", transport_data.keys())
 distance = get_float("Miles per day: ")
+#calc - transport emissions
 transport_emissions = calculate_transport(transport_data, transport, distance)
 print(f'Transportation emissions: {transport_emissions} kg per day')
-
-
-energy = get_valid_input("Energy type used: ", energy_data.keys())
+#user input - shower
 shower_time = get_float("Minutes per shower: ")
-
-# calc
-
 water_emissions = calculate_water(shower_time, 0.2)
+print(f'Shower emissions: {water_emissions} kg per day')
+#use input - energy
+energy = get_valid_input("Energy type used: ", energy_data.keys())
 
-total = transport_emissions + water_emissions
+
+
+
+total = transport_emissions + water_emissions 
 
 # output
 print(f"\nTotal Daily CO₂: {total:.2f} kg")
